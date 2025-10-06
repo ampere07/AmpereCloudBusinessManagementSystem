68968e4fe925618e3f33f7050444fb4dc241af76
ampere07 <ravenampere0123@gmail.com>
2025-10-06 22:08:21 +0800

    ready for production

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,14 +6,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
+    'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:8000',
         'http://127.0.0.1:8000',
         'https://atssfiber.ph',
         'https://www.atssfiber.ph',
-    ]),
+    ],
 
     'allowed_origins_patterns' => [],
 
